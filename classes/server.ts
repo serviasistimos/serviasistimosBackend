5b0c978d0da2912025f1b17fe4fd9d81944f8a13
DanielCendales0821 <DanielCendales0821@gmail.com>
2020-06-09 19:20:45 -0500

    commit

--- classes/server.ts
+++ classes/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 export default class Server {
 
     public app : express.Application;
-    public port: Number = 3000;
+    public port: Number = 3000 || process.env.PORT;
 
     constructor() {
         this.app = express();
