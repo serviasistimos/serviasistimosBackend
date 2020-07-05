1836d72aaf608a2b782d55c847883c5812a062bb
DanielCendales0821 <DanielCendales0821@gmail.com>
2020-07-05 18:25:39 -0500

    only lack the request method

--- routes/technical.ts
+++ routes/technical.ts
@@ -6,11 +6,11 @@ import { verifyToken } from "../middlewares/authentication";
 const technicalRoutes = Router();
 const technicalController = new TechnicalController();
 
-technicalRoutes.post('/createTechnical', [ verifyToken ], ( req: Request, res: Response ) => { technicalController.createTechnical(req, res) });
-technicalRoutes.put('/updateTechnical/:id', [ verifyToken ], ( req: Request, res: Response ) => { technicalController.updateTechnical(req, res) });
-technicalRoutes.delete('/deleteTechnical/:id', [ verifyToken ], ( req: Request, res: Response ) => { technicalController.deleteTechnical(req, res) });
-technicalRoutes.get('/technicals', [ verifyToken ], ( req: Request, res: Response ) => { technicalController.getTechnicals(req, res) });
-technicalRoutes.get('/technical/:id', [ verifyToken ], ( req: Request, res: Response ) => { technicalController.getTechnicalById(req, res) });
+technicalRoutes.post('/createTechnical',  ( req: Request, res: Response ) => { technicalController.createTechnical(req, res) });
+technicalRoutes.put('/updateTechnical/:id',  ( req: Request, res: Response ) => { technicalController.updateTechnical(req, res) });
+technicalRoutes.delete('/deleteTechnical/:id',  ( req: Request, res: Response ) => { technicalController.deleteTechnical(req, res) });
+technicalRoutes.get('/technicals',  ( req: Request, res: Response ) => { technicalController.getTechnicals(req, res) });
+technicalRoutes.get('/technical/:id',  ( req: Request, res: Response ) => { technicalController.getTechnicalById(req, res) });
 
 export default
     technicalRoutes;
